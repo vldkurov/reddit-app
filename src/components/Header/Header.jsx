418996554d48dd5adfad6e5569410d0406b9ad6a
Volodymyr Kurov <151948182+vldkurov@users.noreply.github.com>
2024-01-14 20:30:06 +0000

    Remove App.js component, add Home, Footer, Header components and update dependencies
    The App.js was deleted and replaced with Home.jsx which serves as the new starting page of our application. Two new components, Header and Footer, were added which will be present on every page. Package-lock.json was updated to include new dependencies related to these changes.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import AppBar from '@mui/material/AppBar';
+
+function Header() {
+    return (
+        <header>
+            <AppBar position="static">
+                <h2>RedditMinimal</h2>
+            </AppBar>
+        </header>
+    );
+}
+
+export default Header
