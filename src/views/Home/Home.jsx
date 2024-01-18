3276a053e37e0a51855b3e792769a1cb0a11427d
Volodymyr Kurov <151948182+vldkurov@users.noreply.github.com>
2024-01-18 19:58:45 +0000

    Adjust top property value in Home view
    The top property for medium devices in the Home view has been updated. It was previously at 13.2%, and it has been set to 15% to improve the positioning and visual flow in medium-size displays.

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -42,7 +42,7 @@ function Home() {
                         sx={{
                             position: {xs: 'static', sm: 'fixed', md: 'fixed'},
                             width: {xs: 1, sm: '23%'},
-                            top: {sm: '13.2%', md: '13.2%'},
+                            top: {sm: '13.2%', md: '15%'},
                             zIndex: 1000
                         }}
                     >
