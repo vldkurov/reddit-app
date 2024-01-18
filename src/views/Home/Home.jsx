4b5bc3dc310152e11dec2e26848c4f5562cda656
Volodymyr Kurov <151948182+vldkurov@users.noreply.github.com>
2024-01-18 20:01:11 +0000

    Adjust top positioning on Home view
    The top positioning for medium-sized screens in the Home view has been changed. This adjustment increases the top spacing from 15% to 20%, providing a better layout and view for medium screen resolutions.

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -42,7 +42,7 @@ function Home() {
                         sx={{
                             position: {xs: 'static', sm: 'fixed', md: 'fixed'},
                             width: {xs: 1, sm: '23%'},
-                            top: {sm: '13.2%', md: '15%'},
+                            top: {sm: '13.2%', md: '20%'},
                             zIndex: 1000
                         }}
                     >
